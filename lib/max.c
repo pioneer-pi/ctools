5d5d2857d10ebd896a876cba95b9f268c56dbaf3
pioneer <997851245@qq.com>
2020-10-28 16:22:04 +0800

    all the files in lib

---
+++ lib/max.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<max.h>
+int main()
+{ int a,b;
+a = 10;
+b = 12;
+printf("%d",max(a,b));
+return 0;
+}
+
---
+++ lib/sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<sum.h>
+int main()
+{int a,b;
+a = 10;
+b = 12;
+printf("%d",sum(a,b));
+return 0;
+}
